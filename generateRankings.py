5b73d91ae968d12328f39f25fb0c13e5bce7c4f6
Blaze Alan Marshall <wertercatt@wertercatt.com>
2022-12-17 19:59:57 -0600

    save new ratings, print trainer rankings

--- generateRankings.py
+++ generateRankings.py
@@ -5,7 +5,7 @@ import json
 trainerFile = open("data/trainers.json", "r", encoding="utf-8")
 trainers = json.load(trainerFile)
 trainerFile.close
-# trainerFile = open("data/trainers.json", "w", encoding="utf-8")
+trainerFile = open("data/trainers.json", "w", encoding="utf-8")
 
 
 matchesFile = open("data/matches/trainerBattle/leaderTournament/unityVillage2022-07/results.json", "r", encoding="utf-8")
@@ -14,7 +14,6 @@ matches = json.load(matchesFile)
 for match in matches["matches"]:
     trainer1 = list(match.keys())[0]
     trainer2 = list(match.keys())[1]
-    print(trainers[trainer1]["name"] + " vs " + trainers[trainer2]["name"])
     trainer1OldRating = Rating(mu=trainers[trainer1].pop("mu"), sigma=trainers[trainer1].pop("sigma"))
     trainer2OldRating = Rating(mu=trainers[trainer2].pop("mu"), sigma=trainers[trainer2].pop("sigma"))
     result = [[trainer1NewRating], [trainer2NewRating]] = rate([[trainer1OldRating], [trainer2OldRating],], rank=[match[trainer1], match[trainer2]])
@@ -26,10 +25,10 @@ for match in matches["matches"]:
     trainers[trainer2]["sigma"] = trainer2NewRating.sigma
     trainers[trainer1]["ordinal"] = ordinal([trainer1NewRating.mu, trainer1NewRating.sigma])
     trainers[trainer2]["ordinal"] = ordinal([trainer2NewRating.mu, trainer2NewRating.sigma])
-# json.dump(trainers, trainerFile, sort_keys=True, indent=4)
+json.dump(trainers, trainerFile, sort_keys=True, indent=4)
 trainerRanking = {}
 for trainer in trainers:
-    trainerRanking[trainer["name"]] = trainer["ordinal"]
+    trainerRanking[trainers[trainer]["name"]] = trainers[trainer]["ordinal"]
 trainerRankingSortedKeys = sorted(trainerRanking, key=trainerRanking.get, reverse=True)
 for r in trainerRankingSortedKeys:
     print(r, trainerRanking[r])

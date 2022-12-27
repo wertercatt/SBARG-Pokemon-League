4665f904801fbf773cb094f4690e7682e07a0784
Blaze Marshall <wertercatt@wertercatt.com>
2022-12-27 14:37:03 -0600

    >2 opponents path

--- generateRankings.py
+++ generateRankings.py
@@ -12,28 +12,38 @@ matchesFile = open(sys.argv[1], "r", encoding="utf-8")
 matches = json.load(matchesFile)
 
 for match in matches["matches"]:
-    participants = []
-    oldRatings = []
-    newRatings = []
-    rankings = []
-    for participant in match:
-        tempObject = match[participant]
-        print(json.dumps(tempObject, sort_keys=True, indent=4))
-    # trainer1 = list(match.keys())[0]
-    # trainer2 = list(match.keys())[1]
-    # trainer1OldRating = Rating(mu=trainers[trainer1].pop("mu"), sigma=trainers[trainer1].pop("sigma"))
-    # trainer2OldRating = Rating(mu=trainers[trainer2].pop("mu"), sigma=trainers[trainer2].pop("sigma"))
-    # result = [[trainer1NewRating], [trainer2NewRating]] = rate([[trainer1OldRating], [trainer2OldRating],], rank=[match[trainer1], match[trainer2]])
-    # trainers[trainer1].pop("ordinal")
-    # trainers[trainer2].pop("ordinal")
-    # trainers[trainer1]["mu"] = trainer1NewRating.mu
-    # trainers[trainer1]["sigma"] = trainer1NewRating.sigma
-    # trainers[trainer2]["mu"] = trainer2NewRating.mu
-    # trainers[trainer2]["sigma"] = trainer2NewRating.sigma
-    # trainers[trainer1]["ordinal"] = ordinal([trainer1NewRating.mu, trainer1NewRating.sigma])
-    # trainers[trainer2]["ordinal"] = ordinal([trainer2NewRating.mu, trainer2NewRating.sigma])
-trainerFile = open("data/trainers.json", "w", encoding="utf-8")
-json.dump(trainers, trainerFile, sort_keys=True, indent=4)
+    if len(match) > 2:
+        participants = []
+        oldRankings = []
+        newRankings = []
+        ranks = []
+        for participant in match:
+            participants.append(participant)
+            oldRankings.append([Rating(mu=trainers[participant].pop("mu"), sigma=trainers[participant].pop("sigma"))])
+            ranks.append([match[participant]]) 
+        newRankings = rate(oldRankings, rank=ranks)
+        for idx, x in enumerate(participants):
+            trainers[x].pop("ordinal")
+            trainers[x]["mu"] = newRankings[idx][0].mu
+            trainers[x]["sigma"] = newRankings[idx][0].sigma
+            trainers[x]["ordinal"] = ordinal([newRankings[idx][0].mu, newRankings[idx][0].sigma])
+
+    else:
+        trainer1 = list(match.keys())[0]
+        trainer2 = list(match.keys())[1]
+        trainer1OldRating = Rating(mu=trainers[trainer1].pop("mu"), sigma=trainers[trainer1].pop("sigma"))
+        trainer2OldRating = Rating(mu=trainers[trainer2].pop("mu"), sigma=trainers[trainer2].pop("sigma"))
+        result = [[trainer1NewRating], [trainer2NewRating]] = rate([[trainer1OldRating], [trainer2OldRating],], rank=[match[trainer1], match[trainer2]])
+        trainers[trainer1].pop("ordinal")
+        trainers[trainer2].pop("ordinal")
+        trainers[trainer1]["mu"] = trainer1NewRating.mu
+        trainers[trainer1]["sigma"] = trainer1NewRating.sigma
+        trainers[trainer2]["mu"] = trainer2NewRating.mu
+        trainers[trainer2]["sigma"] = trainer2NewRating.sigma
+        trainers[trainer1]["ordinal"] = ordinal([trainer1NewRating.mu, trainer1NewRating.sigma])
+        trainers[trainer2]["ordinal"] = ordinal([trainer2NewRating.mu, trainer2NewRating.sigma])
+# trainerFile = open("data/trainers.json", "w", encoding="utf-8")
+# json.dump(trainers, trainerFile, sort_keys=True, indent=4)
 trainerRanking = {}
 for trainer in trainers:
     trainerRanking[trainers[trainer]["name"]] = trainers[trainer]["ordinal"]
